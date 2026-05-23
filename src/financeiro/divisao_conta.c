37cb15100f911cc93c3d8c964f8d147e0f78e3b4
Bruno B. <130312444+Forever2B@users.noreply.github.com>
2026-05-23 14:16:56 -0300

    Corrige calculo de gorjeta em "divisao_conta"
    correção do sistema de calculo da gorjeta, nome e iniciação das variáveis.

--- src/financeiro/divisao_conta.c
+++ src/financeiro/divisao_conta.c
@@ -2,12 +2,12 @@
 #include "divisao_conta.h"
 
 void divisao_conta() {
-    	//  vars
+    //  vars
 	float total =0;
-	float totalSgorjeta =0;
+	float totalSemGorjeta =0;
 	int pessoas =0;
 	int gorjeta =0;
-	float contaFinal;
+	float contaFinal = 0;
 	//
 	printf("\nQual o valor total da conta?: ");
 	scanf("%f", &total);
@@ -16,13 +16,13 @@ void divisao_conta() {
 	printf("\nTem gorjeta?: ");
 	scanf("%d", &gorjeta);
 	
-	totalSgorjeta = total;
+	totalSemGorjeta = total;
 	if (gorjeta != 0) {
-		total = total + (total/gorjeta);
+		total = total + (total * gorjeta / 100.0);;
 	};
 	contaFinal = total/pessoas;
 	//
-	printf("\n\nValor da conta: %.2f",totalSgorjeta );
+	printf("\n\nValor da conta: %.2f",totalSemGorjeta);
 	printf("\nGorjeta: %d%%", gorjeta );
 	printf("\nTotal com corjeta: %.2f", total);
 	printf("\nTotal por pessoa: %.2f", contaFinal);
